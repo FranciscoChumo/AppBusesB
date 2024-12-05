7b76fa775c368748c0494ba620f57a2041605e32
FranciscoChumo <frchd.1316334083@gmail.com>
2024-12-05 09:14:05 -0500

    resuelto

--- controller/PersonsController.js
+++ controller/PersonsController.js
@@ -8,11 +8,11 @@ if(!(name||lastname||ci||address||phone)){
     res.status(400).json({ message: "all input is required" });
 }
 const person=await PersonsModel.findOne({where:{id:req.params.id}});
-if(!person){
+if(person){
     person.set({...person,name:name,lastname:lastname, ci:ci,address:address,phone:phone})
     await person.save();
     res.status(200).json({message:"update"});
 }else{
-    res.status(404).json({message: "user not found"});
+    res.status(404).json({message: "user not founds"});
 }
 };
